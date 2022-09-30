3d064120e07f1dfe4286a8c9fcbccd4dc3729e7b
kyumin <kyumin.choi@nhnfashiongo.com>
2022-09-30 16:31:49 +0900

    Feat: apply swaager ui to produce api documents

---
+++ src/main/java/com/droptheclothes/api/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.droptheclothes.api.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title="DroptheClothes API") )
+public class SwaggerConfig {
+
+    private final String SWAGGER_UI_SERVER_PROPERTY_KEY = "springdoc.swagger-ui.server";
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    @Profile({"dev"})
+    public OpenAPI openAPI() {
+        String server = environment.getProperty(SWAGGER_UI_SERVER_PROPERTY_KEY);
+        return new OpenAPI().servers(List.of(new Server().url(server)));
+    }
+}
