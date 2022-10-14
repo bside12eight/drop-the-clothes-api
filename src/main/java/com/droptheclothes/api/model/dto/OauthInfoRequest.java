6a17b4aa33fa862e1db11099a0a59c1ef20d5e51
haenyilee <haenyilee@kakao.com>
2022-10-14 20:52:31 +0900

    Etc: 토큰 저장
    - api/oauth/login : 토큰 저장까지 구현

---
+++ src/main/java/com/droptheclothes/api/model/dto/OauthInfoRequest.java
@@ -0,0 +1,32 @@
+package com.droptheclothes.api.model.dto;
+
+import com.droptheclothes.api.model.entity.Oauth;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class OauthInfoRequest {
+
+  //고유id, 어떤 간편로그인인지, 기기 정보
+  private String authId; // kakao : accesssToken / apple : identity token
+  private String authType; // kakao, apple
+  private String deviceID; //
+
+  @Builder
+  public OauthInfoRequest( String authId, String authType, String deviceID ){
+    this.authId = authId;
+    this.authType = authType;
+    this.deviceID = deviceID;
+  }
+
+  public Oauth toEntity() {
+    return Oauth.builder()
+          .authId(authId)
+          .authType(authType)
+          .deviceID(deviceID)
+        .build();
+  }
+
+}
