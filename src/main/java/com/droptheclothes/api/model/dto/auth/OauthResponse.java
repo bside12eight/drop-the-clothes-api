3e41d4b7ed3ee4d0884b297ad3092a101a8b61f5
hennylee <haenyilee@kakao.com>
2022-11-15 22:48:18 +0900

    Etc: 신규회원 판별 api response에 token 추가

--- src/main/java/com/droptheclothes/api/model/dto/auth/OauthResponse.java
+++ src/main/java/com/droptheclothes/api/model/dto/auth/OauthResponse.java
@@ -6,16 +6,19 @@ import lombok.Getter;
 @Getter
 public class OauthResponse {
 
+  private String accessToken;
   private String nickName;
   private String email;
   private String type;
 
   @Builder
   public OauthResponse(
-        String nickName
+        String accessToken
+      , String nickName
       , String email
       , String type
   ){
+    this.accessToken = accessToken;
     this.nickName = nickName;
     this.email = email;
     this.type = type;
