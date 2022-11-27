f2ab39d1ed82b2d918ecb19dd132aaa5187f2b35
kyumin <kyumin.choi@nhnfashiongo.com>
2022-11-27 13:21:42 +0900

    Refactor: Refactor clothing bin report apis

--- src/main/java/com/droptheclothes/api/service/ClothingBinService.java
+++ src/main/java/com/droptheclothes/api/service/ClothingBinService.java
@@ -3,7 +3,7 @@ package com.droptheclothes.api.service;
 import com.droptheclothes.api.model.dto.clothingbin.ClothingBinResponse;
 import com.droptheclothes.api.model.entity.ClothingBin;
 import com.droptheclothes.api.repository.ClothingBinRepository;
-import com.droptheclothes.api.utility.Constants;
+import com.droptheclothes.api.utility.BusinessConstants;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,16 +15,12 @@ public class ClothingBinService {
     private final ClothingBinRepository clothingBinRepository;
 
     public List<ClothingBinResponse> getClothingBinsWithinRadius(Double latitude, Double longitude) {
-        return clothingBinRepository.getClothingBinsWithinRadius(latitude, longitude, Constants.CLOTHING_BIN_SEARCH_RADIUS);
+        return clothingBinRepository.getClothingBinsWithinRadius(latitude, longitude, BusinessConstants.CLOTHING_BIN_SEARCH_RADIUS);
     }
 
     public ClothingBinResponse getClothingBin(Long clothingBinId) {
         ClothingBin clothingBin = clothingBinRepository.findByClothingBinId(clothingBinId)
                                                        .orElseThrow(() -> new IllegalArgumentException("There is no matched clothing bin"));
         return ClothingBinResponse.entityToDto(clothingBin);
-    }
-
-    public boolean isRegisteredClothingBin(String address) {
-        return clothingBinRepository.findByAddress(address).isPresent();
     }
 }
--- src/main/java/com/droptheclothes/api/utility/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.droptheclothes.api.utility;
-
-public class Constants {
-
-    public static final Integer CLOTHING_BIN_SEARCH_RADIUS = 500;
-}
